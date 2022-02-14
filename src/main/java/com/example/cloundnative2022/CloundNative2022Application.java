d858d9932871a57bc7e566e9d5a82182b2e6617f
mbaye <mbayesarr.baba@gmail.com>
2022-02-14 09:49:22 +0000

    Initial commit

---
+++ src/main/java/com/example/cloundnative2022/CloundNative2022Application.java
@@ -0,0 +1,13 @@
+package com.example.cloundnative2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloundNative2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloundNative2022Application.class, args);
+    }
+
+}
